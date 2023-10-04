9b9de92bd2f3a451f23c5cf64823e4db104a4a85
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-03 21:19:08 -0400

    added reaction model schema

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        rectionId: {
+            type: Schema.Types.ObjectId,
+            default: new ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            //Use a getter method to format the timestamp on query
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+)
