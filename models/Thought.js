de6c4617368d8b303ab4b4f068d59aec127e3a92
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-04 18:07:44 -0400

    added reactionCount virtual

--- models/Thought.js
+++ models/Thought.js
@@ -33,6 +33,10 @@ const thoughtSchema = new Schema(
 
 // Create a virtual called reactionCount that retrieves the length 
 // of the thought's reactions array field on query
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
 
 const Thought = model('thought', thoughtSchema);
 
