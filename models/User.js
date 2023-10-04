15c9a4ddd34e531eaac6385917d01b0a57584498
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-04 18:06:03 -0400

    added friendCount virtual

--- models/User.js
+++ models/User.js
@@ -29,6 +29,9 @@ const userSchema = new Schema(
 
 // Create a virtual called friendCount that retrieves
 // the length of the user's friends array field on query
+userSchema.virtual('friendCount').get(function () {
+    return this.firiends.length;
+  });
 
 const User = model('user', userSchema);
 
