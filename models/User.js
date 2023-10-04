8c5dc1db176421393585199da4e0ef2749b1cc36
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-03 20:29:20 -0400

    added user model schema

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+
+        friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual called friendCount that retrieves
+// the length of the user's friends array field on query
