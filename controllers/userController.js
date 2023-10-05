a50cecae558932aa4714fe544d46ba71880f9f1f
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-04 21:18:25 -0400

    added createUser function

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+ 
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+}
+
+module.exports = userController;
